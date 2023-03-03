4d3e7397787463f8b439286bd214e7db0abb3476
Muhammad Imam Luthfi Balaka <muhammad.imam07@ui.ac.id>
2023-03-03 19:37:35 +0700

    Incorporate staging changes into master (not part of backlog)

--- si_event/urls.py
+++ si_event/urls.py
@@ -1,4 +1,4 @@
-"""project_django URL Configuration
+"""si_event URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
 ]
