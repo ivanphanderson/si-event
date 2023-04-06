c726e2f1ed8308fffb952e95298c9ad6e9e0970b
Zefanya Soplantila <zefanya.soplantila@ui.ac.id>
2023-04-06 14:41:12 +0700

    Implementasi pendaftaran event sesuai dengan yang diharapkan oleh user untuk fungsionalitas menambahkan role dan semacamnya

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import create_event
+from .views import create_event, input_employee_to_event, get_options, get_events
 
 urlpatterns = [
-    path('create', create_event, name='create_event')
+    path('', get_events, name='get_events'),
+    path('create', create_event, name='create_event'),
+    path('input_employee', input_employee_to_event, name='input_employee_to_event'),
+    path('get-options/', get_options, name='get_options'),
 ]
