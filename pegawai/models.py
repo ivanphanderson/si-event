bd6c30b863f9e19615d780d2735fb0f4e5357690
luthfibalaka <luthfibalaka@gmail.com>
2023-04-16 13:53:17 +0700

    [REFACTOR] Improve pegawai app quality

--- pegawai/models.py
+++ pegawai/models.py
@@ -2,29 +2,33 @@ from django.db import models
 
 
 class Pegawai(models.Model):
-    STAFF = 'S'
-    LECTURER = 'L'
-    DOSEN = 'D'
-    ADMINISTRASI = 'A'
-    FUNGSIONAL_TERTENTU = 'FT'
+    STAFF = "S"
+    LECTURER = "L"
+    DOSEN = "D"
+    ADMINISTRASI = "A"
+    FUNGSIONAL_TERTENTU = "FT"
 
     EMPLOYEE_CATEGORY_CHOICES = [
-        (STAFF, 'Staff'),
-        (LECTURER, 'Lecturer'),
+        (STAFF, "Staff"),
+        (LECTURER, "Lecturer"),
     ]
 
     JOB_STATUS_CHOICES = [
-        (DOSEN, 'Dosen'),
-        (ADMINISTRASI, 'Administrasi'),
-        (FUNGSIONAL_TERTENTU, 'Fungsional Tertentu'),
+        (DOSEN, "Dosen"),
+        (ADMINISTRASI, "Administrasi"),
+        (FUNGSIONAL_TERTENTU, "Fungsional Tertentu"),
     ]
 
     email = models.EmailField(primary_key=True)
     employee_no = models.CharField(max_length=18)
     employee_name = models.CharField(max_length=50)
-    employee_category = models.CharField(max_length=50, choices=EMPLOYEE_CATEGORY_CHOICES, default=STAFF)
-    job_status = models.CharField(max_length=50, choices=JOB_STATUS_CHOICES, default=ADMINISTRASI)
-    grade_level = models.CharField(max_length=10, default='-')
+    employee_category = models.CharField(
+        max_length=50, choices=EMPLOYEE_CATEGORY_CHOICES, default=STAFF
+    )
+    job_status = models.CharField(
+        max_length=50, choices=JOB_STATUS_CHOICES, default=ADMINISTRASI
+    )
+    grade_level = models.CharField(max_length=10, default="-")
     employment_status = models.CharField(max_length=50)
     nama_di_rekening = models.CharField(max_length=50)
     nama_bank = models.CharField(max_length=50)
--- pegawai/views.py
+++ pegawai/views.py
@@ -12,23 +12,24 @@ from log.views import add_log
 from account.models import Account
 
 
-URL_AUTH = 'authentication:login'
-FORBIDDEN_PAGE = 'forbidden.html'
+URL_AUTH = "authentication:login"
+FORBIDDEN_PAGE = "forbidden.html"
+
 
 class AddPegawaiView(LoginRequiredMixin, TemplateView):
     login_url = reverse_lazy(URL_AUTH)
 
     def get(self, request, *args, **kwargs):
         if len(Pegawai.objects.all()) > 0:
-            return HttpResponseRedirect(reverse('pegawai:update_pegawai'))
+            return HttpResponseRedirect(reverse("pegawai:update_pegawai"))
         return super(AddPegawaiView, self).get(request, *args, **kwargs)
 
     def get_template_names(self):
         user = self.request.user
         account = Account.objects.get(user=user)
 
-        if account.role == 'Admin':
-            template_name = 'add_pegawai.html'
+        if account.role == "Admin":
+            template_name = "add_pegawai.html"
         else:
             template_name = FORBIDDEN_PAGE
         return [template_name]
@@ -38,51 +39,46 @@ class AddPegawaiView(LoginRequiredMixin, TemplateView):
         account = Account.objects.get(user=user)
 
         context = super().get_context_data(**kwargs)
-        context['role'] = account.role
-        context['title'] = 'Add Employee Data'
+        context["role"] = account.role
+        context["title"] = "Add Employee Data"
 
         return context
 
 
 class SavePegawaiToDatabase(CreateView):
-
-    def check_data(
-        self,
-        data: List[List[str]]
-    ) -> List:
+    def check_data(self, data: List[List[str]]) -> List:
         unique_email_checker = UniqueEmailInFileChecker(None)
-        valid_column_name_checker = ValidColumnNameChecker(
-            unique_email_checker)
+        valid_column_name_checker = ValidColumnNameChecker(unique_email_checker)
         data_cleaner = DataCleaner(valid_column_name_checker)
         return data_cleaner.functionality(data, dict())
 
     def post(self, request, *args, **kwargs):
         try:
-            bytes_file = request.FILES['file'].read()
+            bytes_file = request.FILES["file"].read()
             data: List[List[str]] = convert_to_data(bytes_file)
 
             check_data_result = self.check_data(data)
             data = check_data_result[0]
-            index_start_data = check_data_result[1]['records_index']
+            index_start_data = check_data_result[1]["records_index"]
 
             for index in range(index_start_data, len(data)):
                 new_pegawai = Pegawai(
-                    email = data[index][7].strip(),
-                    employee_no = str(int(data[index][1].strip())),
-                    employee_name = data[index][2].strip(),
-                    employee_category = data[index][3].strip(),
-                    job_status = data[index][4].strip(),
-                    grade_level = data[index][5].strip(),
-                    employment_status = data[index][6].strip(),
-                    nama_di_rekening = data[index][8].strip(),
-                    nama_bank = data[index][9].strip(),
-                    nomor_rekening = str(int(data[index][10].strip())),
-                    nomor_npwp = str(int(data[index][11].strip())),
-                    alamat_npwp = data[index][12].strip(),
+                    email=data[index][7].strip(),
+                    employee_no=str(int(data[index][1].strip())),
+                    employee_name=data[index][2].strip(),
+                    employee_category=data[index][3].strip(),
+                    job_status=data[index][4].strip(),
+                    grade_level=data[index][5].strip(),
+                    employment_status=data[index][6].strip(),
+                    nama_di_rekening=data[index][8].strip(),
+                    nama_bank=data[index][9].strip(),
+                    nomor_rekening=str(int(data[index][10].strip())),
+                    nomor_npwp=str(int(data[index][11].strip())),
+                    alamat_npwp=data[index][12].strip(),
                 )
                 new_pegawai.save()
-            add_log(Account.objects.get(user=self.request.user), 'Add Data Pegawai')
-            return HttpResponseRedirect(reverse('pegawai:display_pegawai'))
+            add_log(Account.objects.get(user=self.request.user), "Add Data Pegawai")
+            return HttpResponseRedirect(reverse("pegawai:display_pegawai"))
         except Exception as e:
             error_message: str = e.args[0]
             user = self.request.user
@@ -90,8 +86,8 @@ class SavePegawaiToDatabase(CreateView):
 
             return TemplateResponse(
                 request,
-                'add_pegawai_error.html', 
-                context={'error': error_message, 'role': account.role}, 
+                "add_pegawai_error.html",
+                context={"error": error_message, "role": account.role},
                 status=400,
             )
 
@@ -104,15 +100,17 @@ class DisplayPegawai(LoginRequiredMixin, TemplateView):
         user = self.request.user
         account = Account.objects.get(user=user)
 
-        if account.role == 'Admin':
-            template_name = 'display_pegawai.html'
+        if account.role == "Admin":
+            template_name = "display_pegawai.html"
         else:
             template_name = FORBIDDEN_PAGE
         return [template_name]
 
     def get_context_data(self, **kwargs):
         IS_NOT_EMPTY = False
-        list_pegawai = Pegawai.objects.all().order_by('employee_no').filter(tombstone=False)
+        list_pegawai = (
+            Pegawai.objects.all().order_by("employee_no").filter(tombstone=False)
+        )
         if len(list_pegawai) != 0:
             IS_NOT_EMPTY = True
 
@@ -120,9 +118,9 @@ class DisplayPegawai(LoginRequiredMixin, TemplateView):
         account = Account.objects.get(user=user)
 
         context = super().get_context_data(**kwargs)
-        context['data'] = list_pegawai
-        context['isNotEmpty'] = IS_NOT_EMPTY
-        context['role'] = account.role
+        context["data"] = list_pegawai
+        context["isNotEmpty"] = IS_NOT_EMPTY
+        context["role"] = account.role
 
         return context
 
@@ -132,15 +130,15 @@ class UpdatePegawaiView(LoginRequiredMixin, TemplateView):
 
     def get(self, request, *args, **kwargs):
         if len(Pegawai.objects.all()) == 0:
-            return HttpResponseRedirect(reverse('pegawai:add_pegawai'))
+            return HttpResponseRedirect(reverse("pegawai:add_pegawai"))
         return super(UpdatePegawaiView, self).get(request, *args, **kwargs)
 
     def get_template_names(self):
         user = self.request.user
         account = Account.objects.get(user=user)
 
-        if account.role == 'Admin':
-            template_name = 'update_pegawai.html'
+        if account.role == "Admin":
+            template_name = "update_pegawai.html"
         else:
             template_name = FORBIDDEN_PAGE
         return [template_name]
@@ -150,36 +148,33 @@ class UpdatePegawaiView(LoginRequiredMixin, TemplateView):
         account = Account.objects.get(user=user)
 
         context = super().get_context_data(**kwargs)
-        context['role'] = account.role
-        context['title'] = 'Update Employee Data'
+        context["role"] = account.role
+        context["title"] = "Update Employee Data"
 
         return context
 
-class SaveUpdatePegawai(TemplateView):
 
-    def check_data(
-        self, 
-        data: List[List[str]]
-    ) -> List:
+class SaveUpdatePegawai(TemplateView):
+    def check_data(self, data: List[List[str]]) -> List:
         unique_email_checker = UniqueEmailInFileChecker(None)
         valid_column_name_checker = ValidColumnNameChecker(unique_email_checker)
         data_cleaner = DataCleaner(valid_column_name_checker)
         return data_cleaner.functionality(data, dict())
-    
+
     def post(self, request, *args, **kwargs):
         try:
-            uploaded_file = request.FILES['file'].read()
+            uploaded_file = request.FILES["file"].read()
             data = convert_to_data(uploaded_file)
 
             check_data_result = self.check_data(data)
             data = check_data_result[0]
-            index_start_data = check_data_result[1]['records_index']
+            index_start_data = check_data_result[1]["records_index"]
 
             self.emails(request, data, index_start_data)
-            
-            add_log(Account.objects.get(user=request.user), 'Update Data Pegawai')
 
-            return HttpResponseRedirect(reverse('pegawai:display_pegawai'))
+            add_log(Account.objects.get(user=request.user), "Update Data Pegawai")
+
+            return HttpResponseRedirect(reverse("pegawai:display_pegawai"))
         except Exception as e:
             error_message: str = e.args[0]
             user = self.request.user
@@ -187,18 +182,18 @@ class SaveUpdatePegawai(TemplateView):
 
             return TemplateResponse(
                 request,
-                'update_pegawai_error.html', 
-                context={'error': error_message, 'role': account.role}, 
+                "update_pegawai_error.html",
+                context={"error": error_message, "role": account.role},
                 status=400,
             )
-        
+
     def emails(self, request, data, index):
         set_of_emails = set()
         all_pegawais = Pegawai.objects.all()
 
         for pegawai in all_pegawais:
-            set_of_emails.add(pegawai.email)  
-        for row in range(index, len(data)): 
+            set_of_emails.add(pegawai.email)
+        for row in range(index, len(data)):
             try:
                 email = data[row][7]
                 set_of_emails.remove(email)
@@ -207,25 +202,36 @@ class SaveUpdatePegawai(TemplateView):
 
                     employee_category = data[row][3].strip()
                     self.update_employee_category(pegawai, employee_category)
-                        
+
                     job_status = data[row][4].strip()
                     self.update_job_status(pegawai, job_status)
 
                     Pegawai.objects.filter(email=email).update(
-                        employee_no= str(data[row][1].strip()), employee_name=data[row][2],
-                        grade_level = data[row][5], employment_status = data[row][6],
-                        nama_di_rekening = data[row][8], nama_bank = data[row][9],
-                        nomor_rekening = str(data[row][10].strip()), nomor_npwp = str(data[row][11].strip()),
-                        alamat_npwp = data[row][12], tombstone = False
+                        employee_no=str(data[row][1].strip()),
+                        employee_name=data[row][2],
+                        grade_level=data[row][5],
+                        employment_status=data[row][6],
+                        nama_di_rekening=data[row][8],
+                        nama_bank=data[row][9],
+                        nomor_rekening=str(data[row][10].strip()),
+                        nomor_npwp=str(data[row][11].strip()),
+                        alamat_npwp=data[row][12],
+                        tombstone=False,
                     )
             except KeyError:
                 new_pegawai = Pegawai(
-                    email = data[row][7].strip(), employee_no = str(data[row][1].strip()),
-                    employee_name = data[row][2].strip(), employee_category = data[row][3].strip(),
-                    job_status = data[row][4].strip(), grade_level = data[row][5].strip(),
-                    employment_status = data[row][6].strip(), nama_di_rekening = data[row][8].strip(),
-                    nama_bank = data[row][9].strip(), nomor_rekening = str(data[row][10].strip()),
-                    nomor_npwp = str(data[row][11].strip()), alamat_npwp = data[row][12].strip(),
+                    email=data[row][7].strip(),
+                    employee_no=str(data[row][1].strip()),
+                    employee_name=data[row][2].strip(),
+                    employee_category=data[row][3].strip(),
+                    job_status=data[row][4].strip(),
+                    grade_level=data[row][5].strip(),
+                    employment_status=data[row][6].strip(),
+                    nama_di_rekening=data[row][8].strip(),
+                    nama_bank=data[row][9].strip(),
+                    nomor_rekening=str(data[row][10].strip()),
+                    nomor_npwp=str(data[row][11].strip()),
+                    alamat_npwp=data[row][12].strip(),
                 )
                 self.update_employee_category(new_pegawai, data[row][3].strip())
                 self.update_job_status(new_pegawai, data[row][4].strip())
@@ -236,20 +242,20 @@ class SaveUpdatePegawai(TemplateView):
                 Pegawai.objects.filter(email=email).update(tombstone=True)
 
     def update_employee_category(self, pegawai, employee_category):
-        if employee_category == 'Lecturer':
+        if employee_category == "Lecturer":
             pegawai.employee_category = pegawai.LECTURER
-        elif employee_category == 'Staff':
-            pegawai.employee_category = pegawai.STAFF 
+        elif employee_category == "Staff":
+            pegawai.employee_category = pegawai.STAFF
         else:
             pegawai.employee_category = pegawai.STAFF
         pegawai.save()
-    
+
     def update_job_status(self, pegawai, job_status):
-        if job_status == 'Dosen':
+        if job_status == "Dosen":
             pegawai.job_status = pegawai.DOSEN
-        elif job_status == 'Fungsional Tertentu':
+        elif job_status == "Fungsional Tertentu":
             pegawai.job_status = pegawai.FUNGSIONAL_TERTENTU
-        elif job_status == 'Administrasi':
+        elif job_status == "Administrasi":
             pegawai.job_status = pegawai.ADMINISTRASI
         else:
             pegawai.job_status = pegawai.ADMINISTRASI
